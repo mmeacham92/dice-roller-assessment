97db9e6b87817f224569e52e8718ca2379dcda10
meachamm92 <meacham.space@gmail.com>
2020-10-08 10:47:44 -0500

    added more game logic, styled the die element, added some css

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,9 +4,11 @@ let totalScore = document.querySelector("#totalScore");
 const rollButton = document.querySelector("#rollButton");
 const showRolls = document.querySelector("#showRolls");
 let rollsList = document.querySelector("#rollsList");
+let numSides = document.querySelector("#numSides");
+let errorMessage = document.querySelector("#error");
+let rollsShown = false;
 
 let dieRolls = []; // will record die roll values here
-let rollsShown = false;
 
 // event listeners
 rollButton.addEventListener("click", rollDie);
@@ -14,37 +16,47 @@ showRolls.addEventListener("click", showAllRolls);
 
 // functions
 function rollDie() {
-  if (rollButton.innerHTML.toLowerCase() === "reset") reset();
-  else {
+  if (rollButton.innerHTML.toLowerCase() === "reset") {
+    reset();
+  } else if (numDice.value !== ""){
+    // reset error message if necessary
+    errorMessage.innerHTML = "";
     // number of rolls
     let count = Number(numDice.value);
+    // number of sides
+    let sides = Number(numSides.value);
+    if (sides === 0) sides = 6;
     // will be our total score value
     let sum = 0;
 
     while (count > 0) {
-      // generate random number 1-6
-      let roll = Math.ceil(Math.random() * 6);
+      // generate random number based on number of sides
+      let roll = Math.ceil(Math.random() * sides);
       // adding roll to the total score
       sum += roll;
       // add roll item to the dieRolls array
       dieRolls.push(roll);
       // decrease count value by 1 (decrement)
       count--;
-    }
+    } 
     // show sum as total score on the page
     totalScore.innerHTML = sum;
+    // change "roll" button into "reset" button
     rollButton.innerHTML = "Reset";
-  }
+    rollButton.style.backgroundColor = "red";
+  } else errorMessage.innerHTML = "Please enter a number of dice to roll!";
 }
 
 function reset() {
   dieRolls = [];
   totalScore.innerHTML = "";
   numDice.value = "";
+  numSides.value = "";
   rollsList.innerHTML = "";
   sum = 0;
   rollsShown = false;
   rollButton.innerHTML = "Roll!";
+  rollButton.style.backgroundColor = "blue";
 }
 
 function showAllRolls() {
@@ -52,8 +64,8 @@ function showAllRolls() {
     let index = 0;
     while (index < dieRolls.length) {
       // insert each item of the array into the Ordered List
-      rollsList.innerHTML += `<li><img src="./assets/images/${dieRolls[index]}.png"</li>`;
-      // increment index (so we don't have an infinite loop)
+      rollsList.innerHTML += `<li><div class="die"><span class="dieValue">${dieRolls[index]}</span></div></li>`;
+      // increment index
       index++;
     }
     rollsShown = true;
